cfea95474f3fbc1dc8bed6b2935cdd7022861776
Shikhar Agrawal <shikharagrawal26@gmail.com>
2026-06-11 12:10:55 +0530

    Add Day_11

---
+++ Day_11/p41.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int sum(int a, int b){
+    return a+b;
+}
+int main()
+{
+    int a,b;
+    printf("Enter two numbers: ");
+    scanf("%d %d",&a,&b);
+    printf("The sum of the numbers is: %d",sum(a,b));
+    return 0;
+}
